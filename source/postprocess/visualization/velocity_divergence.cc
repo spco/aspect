c6626a918d9017348ba6488aca9c3a94a03b5fba
SAM COX <spc29@le.ac.uk>
2016-04-28 15:07:45 +0100

    Completed two new visualization postprocessros: velocity divergence does what it says, at each point. velocity div mag ratio calculates div u/(|u|^2) at each point. Each seems to require very little computation.

---
+++ source/postprocess/visualization/velocity_divergence.cc
@@ -0,0 +1,88 @@
+/*
+  Copyright (C) 2011 - 2015 by the authors of the ASPECT code.
+
+  This file is part of ASPECT.
+
+  ASPECT is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2, or (at your option)
+  any later version.
+
+  ASPECT is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with ASPECT; see the file doc/COPYING.  If not see
+  <http://www.gnu.org/licenses/>.
+*/
+
+
+#include <aspect/postprocess/visualization/velocity_divergence.h>
+#include <aspect/simulator_access.h>
+
+
+
+namespace aspect
+{
+  namespace Postprocess
+  {
+    namespace VisualizationPostprocessors
+    {
+      template <int dim>
+      VelocityDivergence<dim>::
+      VelocityDivergence ()
+        :
+        DataPostprocessorScalar<dim> ("velocity_divergence",
+                                      update_gradients | update_q_points)
+      {}
+
+
+
+      template <int dim>
+      void
+      VelocityDivergence<dim>::
+      compute_derived_quantities_vector (const std::vector<Vector<double> > &,
+                                         const std::vector<std::vector<Tensor<1,dim> > > &duh,
+                                         const std::vector<std::vector<Tensor<2,dim> > > &,
+                                         const std::vector<Point<dim> > &,
+                                         const std::vector<Point<dim> > &,
+                                         std::vector<Vector<double> >                    &computed_quantities) const
+      {
+        const unsigned int n_quadrature_points = duh.size();
+        Assert (computed_quantities.size() == n_quadrature_points,    ExcInternalError());
+        Assert (computed_quantities[0].size() == 1,                   ExcInternalError());
+        Assert (duh[0].size() == this->introspection().n_components,          ExcInternalError());
+
+        for (unsigned int q=0; q<n_quadrature_points; ++q)
+          {
+            // sum up diagonal elements of gradient vector
+            double div_u=0.;
+            for (unsigned int d=0; d<dim; ++d)
+              div_u += duh[q][d][d];
+
+            // output div u
+            computed_quantities[q](0) = div_u;
+          }
+      }
+    }
+  }
+}
+
+
+// explicit instantiations
+namespace aspect
+{
+  namespace Postprocess
+  {
+    namespace VisualizationPostprocessors
+    {
+      ASPECT_REGISTER_VISUALIZATION_POSTPROCESSOR(VelocityDivergence,
+                                                  "velocity divergence",
+                                                  "A visualization output object that generates output "
+                                                  "for the divergence of the velocity field, i.e., for the quantity "
+                                                  "$\\sum_{i=1}^{\\mathrm{dim}}\\frac{\\partial{\\mathbf u}_i}{\\partial{x_i}}$ ")
+    }
+  }
+}
